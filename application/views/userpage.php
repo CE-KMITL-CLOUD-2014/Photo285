7b38d6f292a913a2c4ea2e95cd63745587ba20f2
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-23 16:18:10 +0700

    test

--- application/views/userpage.php
+++ application/views/userpage.php
@@ -45,7 +45,7 @@
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 		
 		
 		<div class="col-xs-2 col-md-1">
