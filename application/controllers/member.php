72d121c706e90456275e2f0e95b3beec984b0516
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-23 18:04:21 +0700

    test

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -4,9 +4,9 @@ class Member extends CI_Controller {
 	public function register(){
 			$IDmember = $_POST["IDmember"];
 			$name = $_POST["name"];
-			$password = md5($_POST["password"]);
+			$pass = md5($_POST["pass"]);
 			$info = $_POST["info"];
-			$data = array('IDmember'=>$IDmember,'name'=>$name,'password'=>$password,'info' =>$info);
+			$data = array('IDmember'=>$IDmember,'name'=>$name,'pass'=>$pass,'info' =>$info);
 			$this->load->model('member_model');	
 			$this->member_model->register($data);
 		}
