e09276f64b44585dff8ec90a3db93f6b6468d505
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-23 23:16:38 +0700

    test

--- application/controllers/photo.php
+++ application/controllers/photo.php
@@ -6,9 +6,9 @@ class Photo extends CI_Controller {
 	public function addphoto(){
 		echo "5555";
 		$IDphoto = $_POST["IDphoto"];
-		//$nameAlbum = $_POST["nameAlbum"];
-		//$photo[] = $_POST["file_upload[]"];
-		//$data = array('IDphoto'=>$IDphoto,'nameAlbum'=>$nameAlbum,'photo[]'=>$file_upload[]);
+		$nameAlbum = $_POST["nameAlbum"];
+		$photo[] = $_POST["file_upload[]"];
+		$data = array('IDphoto'=>$IDphoto,'nameAlbum'=>$nameAlbum,'photo[]'=>$file_upload[]);
 		//$this->load->model('photo_model');	
 		//$this->photo_model->addphoto($data);
 		//$timeupload = date("Y-m-d H:i:s");*/
