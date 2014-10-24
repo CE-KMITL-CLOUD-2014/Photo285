b8c01f1cafeeaed34defc3c4c08b4b66c43badbf
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-25 03:06:41 +0700

    fixed

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -4,9 +4,6 @@
 </head>
 <body>
 
-<?php echo $error;?>
-
-<?php echo form_open_multipart('upload/do_upload');?>
 <form action="../Upload/do_upload">
 <input type="file" name="userfile" size="20" />
 
