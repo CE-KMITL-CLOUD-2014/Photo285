eb014cdda0a71fb65cbe75d4bb80bb3a28f5b46b
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-25 03:01:55 +0700

    test

---
+++ application/controllers/upload.php
@@ -0,0 +1,51 @@
+<?php
+
+class Upload extends CI_Controller {
+$config['upload_path'] = './uploads/';
+$config['allowed_types'] = 'gif|jpg|png';
+$config['max_size']	= '100';
+$config['max_width'] = '1024';
+$config['max_height'] = '768';
+
+$this->load->library('upload', $config);
+
+// Alternately you can set preferences by calling the initialize function. Useful if you auto-load the class:
+$this->upload->initialize($config);
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(array('form', 'url'));
+	}
+
+	function index()
+	{
+		$this->load->view('upload_form', array('error' => ' ' ));
+	}
+
+	function do_upload()
+	{	
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '100';
+		$config['max_width']  = '1024';
+		$config['max_height']  = '768';
+		
+		$this->load->library('upload', $config);
+		
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_form', $error);
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+
+			$this->load->view('upload_success', $data);
+		}
+		$field_name = "some_field_name";
+		$this->upload->do_upload($field_name)
+	}
+}
+?>
