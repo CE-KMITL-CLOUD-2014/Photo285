ea4ec9d64fc1d802b3f38f74099b667122bebd51
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-25 22:06:00 +0700

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -1,52 +1,29 @@
 <?php
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
 
 class Upload extends CI_Controller {
-$config['upload_path'] = './uploads/';
-$config['allowed_types'] = 'gif|jpg|png';
-$config['max_size']	= '100';
-$config['max_width'] = '1024';
-$config['max_height'] = '768';
 
-$this->load->library('upload', $config);
-
-// Alternately you can set preferences by calling the initialize function. Useful if you auto-load the class:
-$this->upload->initialize($config);
-	function __construct()
-	{
-		parent::__construct();
-		$this->load->helper(array('form', 'url'));
-	}
-
-	function index()
-	{
-		$this->load->view('upload_form', array('error' => ' ' ));
-	}
-
-	function do_upload()
-	{	
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '100';
-		$config['max_width']  = '1024';
-		$config['max_height']  = '768';
+	 public function __construct()
+   {
+        parent::__construct();
+   }
+  
+  //if index is loaded
+	public function index() {
+		//load the helper library
+		$this->load->helper('form');
+    $this->load->helper('url');
+		//Set the message for the first time
+		$data = array('msg' => "Upload File");
+    
+    $data['upload_data'] = '';
+    
+		//load the view/upload.php with $data
+		$this->load->view('upload', $data);
+    
 		
-		$this->load->library('upload', $config);
-		
-		if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-
-			$this->load->view('upload_form', $error);
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-
-			$this->load->view('upload_success', $data);
-			echo"Success";
-		}
-		$field_name = "some_field_name";
-		$this->upload->do_upload($field_name)
 	}
+
+
 }
-?>
