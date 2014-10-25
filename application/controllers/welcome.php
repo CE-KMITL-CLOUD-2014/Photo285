f740dd7e0b42881114429332d25ccfb2265e3840
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-25 23:20:00 +0700

    test

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -21,6 +21,10 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->view('mainsite');
 	}
+	public function upload()
+	{
+		$this->load->view('upload');
+	}
 }
 
 /* End of file welcome.php */
