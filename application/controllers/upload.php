e19533a84c487846abac33b22fd83dd480e487db
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-26 01:56:53 +0700

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -10,7 +10,7 @@ $this->config =  array(
                   'max_width'       => "1024"  
                 );
 				$this->load->library('upload', $this->config);
-	rename($this->upload->data()['file_name']), 'TINN'.$this->upload->data()['file_ext']); 				
+	rename(($this->upload->data())['file_name']), 'TINN'.($this->upload->data())['file_ext']); 				
 				if($this->upload->do_upload())
 {
     echo "file upload success";
