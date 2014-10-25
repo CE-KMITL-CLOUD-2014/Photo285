6994f00e72269dc81665f7a16a88d87ba75f0cc2
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-26 01:34:34 +0700

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -2,6 +2,7 @@
 class Upload extends CI_Controller {
 public function uploaded(){
 $this->config =  array(
+				  'file_name'    	=> mypic.jpg
                   'upload_path'     => "./files/",
                   'allowed_types'   => "gif|jpg|png|jpeg",
                   'overwrite'       => TRUE,
