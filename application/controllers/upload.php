082d4d4d9d852eaed73c8c8e6c1adfe5d858b818
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-26 01:01:40 +0700

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -3,14 +3,14 @@ class Upload extends CI_Controller {
 public function uploaded(){
 $this->config =  array(
                   'upload_path'     => "./files/",
-                  'allowed_types'   => "gif|jpg|png|jpeg|pdf|doc|xml",
+                  'allowed_types'   => "gif|jpg|png|jpeg",
                   'overwrite'       => TRUE,
                   'max_size'        => "1000KB",
                   'max_height'      => "768",
                   'max_width'       => "1024"  
                 );
 				$this->load->library('upload', $this->config);
-rename($data['file_name'], 'TINN'.$data['file_ext']); 			
+rename($this->upload->data['file_name'], 'TINN'.$this->upload->data['file_ext']); 			
 if($this->upload->do_upload())
 {
     echo "file upload success";
