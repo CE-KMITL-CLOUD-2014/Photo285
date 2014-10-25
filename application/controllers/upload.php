7750ba54e29fe1db64da34cb9184e8f4adaa7f44
Tinn Thongkukiattikool <tinn.ooppo@hotmail.com>
2014-10-25 22:17:41 +0700

    test

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -1,29 +1,24 @@
 <?php
-if (!defined('BASEPATH'))
-  exit('No direct script access allowed');
-
 class Upload extends CI_Controller {
-
-	 public function __construct()
-   {
-        parent::__construct();
-   }
-  
-  //if index is loaded
-	public function index() {
-		//load the helper library
-		$this->load->helper('form');
-    $this->load->helper('url');
-		//Set the message for the first time
-		$data = array('msg' => "Upload File");
-    
-    $data['upload_data'] = '';
-    
-		//load the view/upload.php with $data
-		$this->load->view('upload', $data);
-    
-		
+	funtion index(){
+		$this->config =  array(
+					  'upload_path'     => "./files/",
+					  'allowed_types'   => "jpg|png|jpeg",
+					  'overwrite'       => TRUE,
+					  'max_size'        => "1000KB",
+					  'max_height'      => "768",
+					  'max_width'       => "1024"  
+					);
+					
+		$this->load->library('upload', $this->config);
+		if($this->upload->do_upload())
+		{
+			echo "file upload success";
+		}
+		else
+		{
+		   echo "file upload failed";
+		}
 	}
-
-
 }
+?>
